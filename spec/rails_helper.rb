21943fa9d2bcfa78f89105594896fefa906de540
Moacir Petry <m.petry9@gmail.com>
2021-10-06 14:41:31 -0300

    cover all tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rails'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -53,6 +54,8 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include Capybara::DSL
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
