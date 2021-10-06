6a28df1067813fd23840fcf58df987607b9803d4
Moacir Petry <m.petry9@gmail.com>
2021-10-06 14:08:54 -0300

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,14 @@ module ContactImporter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: true,
+        controller_specs: true,
+        helper_specs: true,
+        routing_specs: true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
