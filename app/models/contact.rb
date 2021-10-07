567c9f6d58199fcf92532aa441d72b5408781cd5
Moacir Petry <m.petry9@gmail.com>
2021-10-07 11:35:58 -0300

    setup franchise

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,13 +3,13 @@ require 'csv'
 class Contact < ApplicationRecord
   belongs_to :user
   validates :name, :date_of_birth, :phone, :address, :credit_card, :franchise, :email, :user, presence: true
+  validates :name, format: { with: /\A[[:alnum:] -]+\z/ }
   validates :credit_card, credit_card_number: {brands: [:amex, :diners, :discover, :jcb, :mastercard, :visa]} 
 
   def self.import(file, current_user)
     CSV.foreach(file.path, headers: true) do |row|
       detector = CreditCardValidations::Detector.new(row[4])
       franchise = detector.brand
-      # Contact.create! row.to_hash
       Contact.create(
         name: row[0],
         date_of_birth: row[1],
