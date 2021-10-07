11bbdd26e6ca3927ca978e3857b13480d4449fe6
Moacir Petry <m.petry9@gmail.com>
2021-10-07 11:56:04 -0300

    fix credit card test

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -4,7 +4,9 @@ FactoryBot.define do
     date_of_birth { "2021-10-06" }
     phone { '(+57) 320-432-05-09' }
     address { Faker::Address.country }
-    credit_card { Faker::Business.credit_card_number }
+    # FIXME it is broken
+    # credit_card { Faker::Finance.credit_card(:mastercard, :visa) }
+    credit_card { '4111111111111111' }
     franchise { Faker::Business.credit_card_type }
     email { Faker::Internet.email }
     user
