4bbea4a89728008015871f34f6e56f15f16f29a7
Moacir Petry <m.petry9@gmail.com>
2021-10-07 08:32:31 -0300

    add credit card validation by industry

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,9 +3,12 @@ require 'csv'
 class Contact < ApplicationRecord
   belongs_to :user
   validates :name, :date_of_birth, :phone, :address, :credit_card, :franchise, :email, :user, presence: true
- 
+  validates :credit_card, credit_card_number: {brands: [:amex, :diners, :discover, :jcb, :mastercard, :visa]} 
+
   def self.import(file, current_user)
     CSV.foreach(file.path, headers: true) do |row|
+      detector = CreditCardValidations::Detector.new(row[4])
+      franchise = detector.brand
       # Contact.create! row.to_hash
       Contact.create(
         name: row[0],
@@ -13,7 +16,7 @@ class Contact < ApplicationRecord
         phone: row[2],
         address: row[3],
         credit_card: row[4],
-        franchise: 'Master Card',
+        franchise: franchise,
         email: row[5],
         user: current_user
       )
